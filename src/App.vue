6ce7b1a0ec151b81012f67ce49cdc7bf5bf3cc66
tsuchimoto_yusuke <tsuchimoto_yusuke@comsq.com>
2024-04-15 20:14:03 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+import FoodCard from './components/FoodCard.vue'
+import { ref } from 'vue'
+
+const foods = ref([
+  {
+    name: '寿司',
+    emoji: '🍣'
+  },
+  {
+    name: 'ハンバーガー',
+    emoji: '🍔'
+  },
+  {
+    name: 'ラーメン',
+    emoji: '🍜'
+  },
+  {
+    name: 'ピザ',
+    emoji: '🍕'
+  },
+  {
+    name: 'カレー',
+    emoji: '🍛'
+  },
+  {
+    name: '焼肉',
+    emoji: '🥩'
+  }
+])
+
+const pickedFoodIndex = ref(0)
+const newFoodIndex = ref(1)
+
+function pickFood(index) {
+  pickedFoodIndex.value = index
+  newFoodIndex.value++
+}
+</script>
+
+<template>
+  <main>
+    <h1>今日に食べる？</h1>
+    <!-- 属性にrefを使いたいのでv-bindを使用 -->
+    <FoodCard
+      :name="foods[pickedFoodIndex].name"
+      :emoji="foods[pickedFoodIndex].emoji"
+      @click="pickFood(pickedFoodIndex)"
+    />
+    <template v-if="newFoodIndex < foods.length">
+      <p>VS</p>
+      <FoodCard
+        :name="foods[newFoodIndex].name"
+        :emoji="foods[newFoodIndex].emoji"
+        @click="pickFood(newFoodIndex)"
+      />
+    </template>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 350px;
+  margin: 100px auto;
+  text-align: center;
+}
+</style>
---
+++ src/components/FoodCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  emoji: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="card">
+    <div>{{ name }}</div>
+    <div>{{ emoji }}</div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.4);
+  border-radius: 5px;
+  font-size: 2rem;
+  padding: 16px;
+  cursor: pointer;
+}
+
+.title {
+  margin-bottom: 60px;
+}
+</style>
